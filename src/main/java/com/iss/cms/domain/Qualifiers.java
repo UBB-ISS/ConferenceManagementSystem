2b3ecfe2f5ad1bb1326df4c7fb2e23ccca71cd2a
Ioana Serban <ioana_serban_11@yahoo.com>
2021-04-27 18:47:03 +0300

    started domain classes

---
+++ src/main/java/com/iss/cms/domain/Qualifiers.java
@@ -0,0 +1,5 @@
+package com.iss.cms.domain;
+
+public enum Qualifiers {
+    STRONG_ACCEPT, ACCEPT, WEAK_ACCEPT, BORDERLINE, WEAK_REJECT, REJECT, STRONG_REJECT
+}
---
+++ src/main/java/com/iss/cms/domain/User.java
@@ -0,0 +1,23 @@
+package com.iss.cms.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private  String email;
+    private String password;
+}
