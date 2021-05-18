03b9523959e87163fc7b102067ab975af9b3d421
George <sgie2817@sca.ubbcluj.ro>
2021-05-19 01:07:52 +0300

    added services to core

---
+++ core/src/main/java/com/iss/cms/core/repository/PaperSectionRepository.java
@@ -0,0 +1,6 @@
+package com.iss.cms.core.repository;
+
+import com.iss.cms.core.domain.PaperSection;
+
+public interface PaperSectionRepository extends Repository<PaperSection, Integer> {
+}
---
+++ core/src/main/java/com/iss/cms/core/service/IPaperService.java
@@ -0,0 +1,15 @@
+package com.iss.cms.core.service;
+
+import com.iss.cms.core.domain.Paper;
+import com.iss.cms.core.exceptions.CMSException;
+
+import java.util.List;
+
+public interface IPaperService {
+    List<Paper> getPapers();
+
+    Paper findPaperByID(int id) throws CMSException;
+
+    void addPaper(int userConferenceId, String title, String keywords,
+                  String paperText, String abstractText, boolean finalized, boolean accepted) throws CMSException;
+}
